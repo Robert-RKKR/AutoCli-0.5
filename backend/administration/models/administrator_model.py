4fb753f1142b522616c938be7c68e83494ba02d6
Robert-RKKR <64611240+Robert-RKKR@users.noreply.github.com>
2022-02-13 00:53:46 +0100

    Administration

--- backend/administration/models/administrator_model.py
+++ backend/administration/models/administrator_model.py
@@ -1,3 +1,6 @@
+# Django language import:
+from django.utils.translation import gettext_lazy as _
+
 # Django Import:
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
@@ -6,6 +9,7 @@ from django.db import models
 # Validators Import:
 from ..validators import UsernameValueValidator
 from ..validators import PasswordValueValidator
+from ..validators import NameValueValidator
 
 # Application Import:
 from ..managers import AdministratorManager
@@ -20,6 +24,7 @@ class Administrator(BaseModel, AbstractBaseUser, PermissionsMixin):
     # Model validators:
     username_validator = UsernameValueValidator()
     password_validator = PasswordValueValidator()
+    name_validator = NameValueValidator()
 
     # Last login value:
     last_login = models.DateTimeField(blank=True, null=True)
@@ -34,43 +39,86 @@ class Administrator(BaseModel, AbstractBaseUser, PermissionsMixin):
 
     # Basic model valuse:
     username = models.CharField(
+        verbose_name=_('Username'),
+        help_text=_('Administrator username.'),
         max_length=32,
         blank=False,
         unique=True,
         validators=[username_validator],
         error_messages={
-            'null': 'Username field is mandatory.',
-            'blank': 'Username field is mandatory.',
-            'unique': 'Administrator with this username already exists.',
-            'invalid': 'Enter the correct username value. It must contain 4 to 32 digits, letters and spaces.',
+            'null': _('Username field is mandatory.'),
+            'blank': _('Username field is mandatory.'),
+            'unique': _('Administrator with this username already exists.'),
+            'invalid': _('Enter the correct username value. It must contain 4 to 32 digits, letters and spaces.'),
         },
     )
     password = models.CharField(
+        verbose_name=_('Password'),
+        help_text=_('Administrator password.'),
         max_length=128,
         validators=[password_validator],
         error_messages={
-            'null': 'Password field is mandatory.',
-            'blank': 'Password field is mandatory.',
-            'invalid': 'Enter the correct password value. It requires at least one lowercase letter, one uppercase letter, one digit and one special character (@$!%*?&), minimum 8 characters.',
+            'null': _('Password field is mandatory.'),
+            'blank': _('Password field is mandatory.'),
+            'invalid': _('Enter the correct password value. It requires at least one lowercase letter, one uppercase letter, one digit and one special character (@$!%*?&), minimum 8 characters.'),
         },
     )
     email = models.EmailField(
+        verbose_name=_('E-mail'),
+        help_text=_('Administrator e-mail address.'),
         max_length=128,
         unique=True,
         blank=True,
         null=True,
         error_messages={
-            'null': 'E-mail field is mandatory.',
-            'blank': 'E-mail field is mandatory.',
-            'unique': 'Administrator with this e-mail already exists.',
-            'invalid': 'Enter the correct e-mail value.',
+            'null': _('E-mail field is mandatory.'),
+            'blank': _('E-mail field is mandatory.'),
+            'unique': _('Administrator with this e-mail already exists.'),
+            'invalid': _('Enter the correct e-mail value.'),
         },
     )
 
     # Personal values:
-    first_name = models.CharField(max_length=32)
-    middle_name = models.CharField(max_length=128)
-    last_name = models.CharField(max_length=128)
+    first_name = models.CharField(
+        verbose_name=_('First name'),
+        help_text=_('Administrator first name.'),
+        max_length=32,
+        blank=True,
+        null=True,
+        validators=[name_validator],
+        error_messages={
+            'null': _('First name field is mandatory.'),
+            'blank': _('First name field is mandatory.'),
+            'unique': _('First name with this name already exists.'),
+            'invalid': _('Enter the correct first name value. It must contain 4 to 32 digits, letters and special characters -, _ or spaces.'),
+        },
+    )
+    middle_name = models.CharField(
+        verbose_name=_('Middle name'),
+        help_text=_('Administrator middle name.'),
+        blank=True,
+        null=True,
+        validators=[name_validator],
+        error_messages={
+            'null': _('Middle name field is mandatory.'),
+            'blank': _('Middle name field is mandatory.'),
+            'unique': _('Middle name with this name already exists.'),
+            'invalid': _('Enter the correct middle name value. It must contain 4 to 32 digits, letters and special characters -, _ or spaces.'),
+        },
+    )
+    last_name = models.CharField(
+        verbose_name=_('Last name'),
+        help_text=_('Administrator last name.'),
+        blank=True,
+        null=True,
+        validators=[name_validator],
+        error_messages={
+            'null': _('Last name field is mandatory.'),
+            'blank': _('Last name field is mandatory.'),
+            'unique': _('Last name with this name already exists.'),
+            'invalid': _('Enter the correct last name value. It must contain 4 to 32 digits, letters and special characters -, _ or spaces.'),
+        },
+    )
 
     # User Constants:
     USERNAME_FIELD = 'username'
