216b00441ebcb725681a65ce0bb024fb73c337b9
robert-rkkr <robert.kucharski.rkkr@gmail.com>
2022-03-09 23:24:59 +0100

    View model - create

--- backend/inventory/views/test.py
+++ backend/inventory/views/test.py
@@ -1,17 +1,7 @@
-# Django Imports:
-from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.views.generic import View
-from django.shortcuts import render
-from django import forms
-
-# Models Imports:
-from logger.models.log_details_model import LogDetails
-from logger.models.log_model import Log
-
 # Application Import:
 from inventory.models.device_model import Device
 from inventory.filters.device_filter import DeviceFilter
-from logger.logger import Logger
+from inventory.forms.device_form import DeviceForm
 
 # Own views models:
 from autocli.baseview.views.list_view import ListView
@@ -19,14 +9,6 @@ from autocli.baseview.views.detail_view import DetailView
 from autocli.baseview.views.update_view import UpdateView
 from autocli.baseview.views.delete_view import DeleteView
 from autocli.baseview.views.create_view import CreateView
-
-
-class AddForm(forms.ModelForm):
-
-    class Meta:
-        model = Device
-        fields = ('active', 'name', 'hostname')
-
 
 class DeviceListView(ListView):
 
@@ -43,4 +25,4 @@ class DeviceDetailView(DetailView):
 class TestCreateView(CreateView):
 
     model = Device
-    form_class = AddForm
+    form_class = DeviceForm
